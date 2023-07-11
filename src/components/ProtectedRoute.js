e9400dd081fc98d5e50a6115ab77c2bccecb7e09
Lusabero <lisaovikyan@gmail.com>
2023-07-11 21:31:14 +0300

    13ПР 2

--- src/components/ProtectedRoute.js
+++
@@ -1,9 +0,0 @@
-import {Navigate} from "react-router-dom";
-
-const ProtectedRoute = ({component: Component, ...props}) => {
-    return (
-        props.loggedIn ? <Component {...props} /> : <Navigate to="./sign-in" replace/>
-    );
-};
-
-export default ProtectedRoute;
